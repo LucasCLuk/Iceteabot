bd22558287010c9bc73264753ac39f82348e5bee
Lucas <lucaslukowski@gmail.com>
2019-01-05 14:43:01 -0700

    added argument parsing. Now takes a name and config file path

--- bot.py
+++ bot.py
@@ -1,9 +1,19 @@
+import argparse
+import os
 import ujson
 
-from iceteabot import Iceteabot
+from utils.iceteabot import Iceteabot
 
-with open("data/config.json") as config_file:
-    config = ujson.load(config_file)
+parser = argparse.ArgumentParser()
+parser.add_argument("name", type=str, help="The name of the bot")
+parser.add_argument("config", type=str, help="Path to config file for the bot")
+args = parser.parse_args()
+
+if os.path.exists(args.config):
+    with open(args.config) as config_file:
+        config = ujson.load(config_file)
+else:
+    raise FileNotFoundError("Config file not found")
 
 bot = Iceteabot(config=config)
 
