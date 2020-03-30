6ad464267c58306f42350539e1ef1ea2efd76b57
Lucas <lucaslukowski@gmail.com>
2020-03-29 18:37:22 -0600

    Added python-dotenv requirement

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import asyncio
 
+import dotenv
+
 from utils.iceteabot import Iceteabot
 
 
 async def main():
+    dotenv.load_dotenv()
     bot = Iceteabot()
     bot.setup_logging()
     await bot.setup_database()
