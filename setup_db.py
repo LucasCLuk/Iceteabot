d8eae0d5bdf46968c2988fa8e5e7f8a99420041d
Lucas <13612932+LucasCLuk@users.noreply.github.com>
2020-06-21 20:08:38 -0600

    fixed setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -1,9 +1,16 @@
+import asyncio
+
 import dotenv
 
 from utils.iceteabot import Iceteabot
 
-if __name__ == '__main__':
+
+async def main():
     dotenv.load_dotenv()
     bot = Iceteabot()
     bot.setup_logging()
     await bot.setup_database()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
